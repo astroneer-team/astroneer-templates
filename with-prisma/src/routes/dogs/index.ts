043031123ac6d0eba2569504abc52c855ce88560
Lucas Larangeira <lukearch@proton.me>
2024-05-08 18:11:01 -0300

    chore: Update package dependencies and add zod library

---
+++ with-prisma/src/routes/dogs/index.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from '@astroneer/core';
+import { z } from 'zod';
+import prisma from '../../prisma/client';
+
+export async function GET(_: Request, res: Response) {
+  const dogs = await prisma.dog.findMany();
+  res.json(dogs);
+}
+
+export async function POST(req: Request, res: Response) {
+  const schema = z.object({
+    name: z.string(),
+    age: z.number(),
+    breed: z.string(),
+  });
+
+  const { success, data, error } = schema.safeParse(req.body);
+
+  if (!success) {
+    return res.status(400).json({ error });
+  }
+
+  const dog = await prisma.dog.create({
+    data: {
+      name: data.name,
+      age: data.age,
+      breed: data.breed,
+    },
+  });
+
+  res.json(dog);
+}
