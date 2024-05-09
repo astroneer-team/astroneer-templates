4a9fb8c3575d1e236e6cf92985b216fbbb0eb843
Lucas Larangeira <lukearch@proton.me>
2024-05-09 11:01:27 -0300

    chore: Refactor server.ts to accept port, hostname, and devmode as parameters

--- with-prisma/src/server.ts
+++ with-prisma/src/server.ts
@@ -3,7 +3,11 @@ import { Astroneer } from '@astroneer/core';
 import { createServer } from 'http';
 import { parse } from 'url';
 
-export default async function server(_: number, __: string, devmode: boolean) {
+export default async function server(
+  port: number,
+  hostname: string,
+  devmode: boolean,
+) {
   const app = await Astroneer.prepare();
   return createServer(async (req, res) => {
     if (devmode) logRequest(req, res);
