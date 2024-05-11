15274a74a5802b8915c21fc93ad52579ee330e19
Lucas Larangeira <lukearch@proton.me>
2024-05-11 04:31:29 -0300

    chore: Update package dependencies to version 1.2.1

--- default/src/server.ts
+++ default/src/server.ts
@@ -1,23 +1,29 @@
-import { logRequest } from '@astroneer/common';
+import { Logger } from '@astroneer/common';
 import { Astroneer } from '@astroneer/core';
 import { createServer } from 'http';
 import { parse } from 'url';
 
-export default async function server(
-  port: number,
-  hostname: string,
-  devmode: boolean,
-) {
-  const app = await Astroneer.prepare();
-  return createServer(async (req, res) => {
-    if (devmode) logRequest(req, res);
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST || 'localhost';
 
+Astroneer.prepare().then((app) => {
+  const server = createServer(async (req, res) => {
     try {
       const parsedUrl = parse(req.url || '', true);
       await app.handle(req, res, parsedUrl);
     } catch (err) {
       console.error(err);
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.statusCode = 500;
+      res.end('Internal Server Error');
     }
   });
-}
+
+  server.once('error', (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  server.listen(Number(port), hostname, () => {
+    Logger.log(`> Listening on http://${hostname}:${port}`);
+  });
+});
