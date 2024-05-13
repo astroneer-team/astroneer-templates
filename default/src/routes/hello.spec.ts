a2c2146b280d2e2cf14cbbac3e346d5f24b6c912
Lucas Larangeira <lukearch@proton.me>
2024-05-13 10:36:37 -0300

    chore: Update package dependencies to version 1.3.1 and fix e2e test for /hello route

---
+++ default/src/routes/hello.spec.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from '@astroneer/core';
+import { GET } from './hello';
+
+describe('Hello', () => {
+  it('should return greeting', () => {
+    const req = {} as Request;
+    const res = {
+      send: jest.fn(),
+    } as unknown as Response;
+
+    GET(req, res);
+
+    expect(res.send).toHaveBeenCalledWith('Hello, from Astroneer! 🚀');
+  });
+});
